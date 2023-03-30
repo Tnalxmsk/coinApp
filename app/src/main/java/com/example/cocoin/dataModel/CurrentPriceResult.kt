38badea3f1648281cd8805e7b41d9ced45c574b0
muldaeng <alstnwkd990@naver.com>
2023-03-30 15:18:14 +0900

    [ADD] 코인 선택화면 API 데이터 가공

---
+++ app/src/main/java/com/example/cocoin/dataModel/CurrentPriceResult.kt
@@ -0,0 +1,6 @@
+package com.example.cocoin.dataModel
+
+data class CurrentPriceResult(
+    val coinName: String,
+    val coinInfo: CurrentPrice
+)
--- app/src/main/java/com/example/cocoin/view/select/SelectViewModel.kt
+++ app/src/main/java/com/example/cocoin/view/select/SelectViewModel.kt
@@ -2,7 +2,10 @@ package com.example.cocoin.view.select
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.cocoin.dataModel.CurrentPrice
+import com.example.cocoin.dataModel.CurrentPriceResult
 import com.example.cocoin.repository.NetWorkRepository
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -10,9 +13,25 @@ class SelectViewModel : ViewModel() {
 
     private val netWorkRepository = NetWorkRepository()
 
+    private lateinit var currentPriceResultList: ArrayList<CurrentPriceResult>
+
     fun getCurrentCoinList() = viewModelScope.launch {
         val result = netWorkRepository.getCurrentCoinList()
 
-        Timber.d(result.toString())
+        currentPriceResultList = ArrayList()
+
+        for (coin in result.data) {
+            try {
+                val gson = Gson()
+                val gsonToJson = gson.toJson(result.data[coin.key])
+                val gsonFromJson = gson.fromJson(gsonToJson, CurrentPrice::class.java)
+
+                val currentPriceResult = CurrentPriceResult(coin.key, gsonFromJson)
+
+                currentPriceResultList.add(currentPriceResult)
+            }catch (e: java.lang.Exception) {
+                Timber.d(e.toString())
+            }
+        }
     }
 }
