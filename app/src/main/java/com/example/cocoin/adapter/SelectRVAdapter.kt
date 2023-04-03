4e7a511831274c4b39b243c268d6774101c75c69
muldaeng <alstnwkd990@naver.com>
2023-04-04 00:16:29 +0900

    [ADD] coin 선택화면 adapter 이미지 매핑

--- app/src/main/java/com/example/cocoin/adapter/SelectRVAdapter.kt
+++ app/src/main/java/com/example/cocoin/adapter/SelectRVAdapter.kt
@@ -1,21 +1,51 @@
 package com.example.cocoin.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cocoin.R
 import com.example.cocoin.dataModel.CurrentPriceResult
 import com.example.cocoin.databinding.ItemIntroCoinBinding
 
 class SelectRVAdapter(val context: Context, private val coinPriceList: List<CurrentPriceResult>) : RecyclerView.Adapter<SelectRVAdapter.SelectViewHolder>() {
     inner class SelectViewHolder(private val binding: ItemIntroCoinBinding) : RecyclerView.ViewHolder(binding.root) {
+        val selectedCoinList = ArrayList<String>()
         fun bind(currentPriceResult: CurrentPriceResult) {
             val fluctate_24H = coinPriceList[position].coinInfo.fluctate_24H
             with(binding) {
                 itemCoinNameTv.text = coinPriceList[position].coinName
-                itemLikeBt
-            }
-            if (fluctate_24H.contains("-")) {
+
+                val currentCoin = coinPriceList[position].coinName
+                if (selectedCoinList.contains(currentCoin)) {
+                    itemLikeBt.setImageResource(R.drawable.like_red)
+                }
+                else {
+                    itemLikeBt.setImageResource(R.drawable.like_grey)
+                }
+                itemLikeBt.setOnClickListener {
+                    // 포함하면
+                    if (selectedCoinList.contains(currentCoin)) {
+                        selectedCoinList.remove(currentCoin)
+                        itemLikeBt.setImageResource(R.drawable.like_grey)
+                    }
+                    // 포함하지 않으면
+                    else {
+                        selectedCoinList.add(currentCoin)
+                        itemLikeBt.setImageResource(R.drawable.like_red)
+                    }
+                }
+
+                // 상승 하락 text
+                if (fluctate_24H.contains("-")) {
+                    itemCoinPriceUpDown.text = "하락입니다."
+                    itemCoinPriceUpDown.setTextColor(Color.parseColor("#114fed"))
+                }
+                else {
+                    itemCoinPriceUpDown.text = "상습입니다."
+                    itemCoinPriceUpDown.setTextColor(Color.parseColor("#ed2e11"))
+                }
             }
 
         }
