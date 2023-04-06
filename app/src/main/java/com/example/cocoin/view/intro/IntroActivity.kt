058d7d952a51cec13213678da5a71456faadac64
muldaeng <alstnwkd990@naver.com>
2023-04-06 15:29:38 +0900

    [ADD] 처음 접속 판단 분기 처리

--- app/src/main/java/com/example/cocoin/view/intro/IntroActivity.kt
+++ app/src/main/java/com/example/cocoin/view/intro/IntroActivity.kt
@@ -1,10 +1,12 @@
 package com.example.cocoin.view.intro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.cocoin.databinding.ActivityIntroBinding
+import com.example.cocoin.view.MainActivity
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
@@ -17,6 +19,16 @@ class IntroActivity : AppCompatActivity() {
 
         with(binding) {
             viewModel.checkFirstFlag()
+            viewModel.first.observe(this@IntroActivity) {
+                if (it) {
+                    // 처음 접속 유저 x
+                    val intent = Intent(this@IntroActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    // 처음 접속 유저
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/cocoin/view/intro/IntroViewModel.kt
+++ app/src/main/java/com/example/cocoin/view/intro/IntroViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.cocoin.view.intro
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cocoin.dataStore.MyDataStore
@@ -7,6 +9,9 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class IntroViewModel : ViewModel() {
+    private val _first = MutableLiveData<Boolean>()
+    val first : LiveData<Boolean>
+        get() = _first
     fun checkFirstFlag() = viewModelScope.launch {
         val getData = MyDataStore().getFirstData()
         Timber.d(getData.toString())
